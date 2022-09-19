8ea85dacb84eb48ea25a85698963fa9dccfbb8a1
George Levov <georgelevov@gmail.com>
2022-09-19 11:56:35 -0700

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+/**
+ * Challenge: build the basic structure of our game
+ *
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in
+ *      (tip: React normalizes <textarea /> to be more like <input />,
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ */
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <h1>Game Title</h1>
+            <textarea
+            // value=""
+            // placeholder=""
+            // onChange={}
+            // name=""
+            />
+
+            <h4>Time Remaining: </h4>
+            <button>Start Game</button>
+
+            <h1>Word Count</h1>
+        </div>
+    );
+}
+
+export default App;
